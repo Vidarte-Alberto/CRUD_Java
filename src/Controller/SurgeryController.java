64984dded4145b710fc0026709a58bd392803cb0
Alberto Vidarte <albertanimations@icloud.com>
2023-10-19 01:03:28 -0600

    Delete Method innecesary

--- src/Controller/SurgeryController.java
+++ src/Controller/SurgeryController.java
@@ -14,16 +14,6 @@ public class SurgeryController {
         surgery.saveToDatabase(); // Guarda la cirugía en la base de datos
     }
 
-    public void updateSurgery(int surgeryId, int patientId, int roomNumber, int doctorId, Date startDate) {
-        Surgery surgery = Surgery.retrieveFromDatabase(surgeryId);
-        if (surgery != null) {
-            surgery.setPatientId(patientId);
-            surgery.setRoomNumber(roomNumber);
-            surgery.setDoctorId(doctorId);
-            surgery.setStartDate(startDate);
-            surgery.saveToDatabase(); // Actualiza la cirugía en la base de datos
-        }
-    }
 
     public void deleteSurgery(int surgeryId) {
         Surgery surgery = Surgery.retrieveFromDatabase(surgeryId);
